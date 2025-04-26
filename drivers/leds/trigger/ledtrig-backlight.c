ef11f51fddace645480f63b4ecf3dd64e4f49783
sonic011gamer <efimgirjeu@gmail.com>
2025-04-26 14:21:28 +0000

    Revert "ether: leds: Import all changes"
    This reverts commit 0f387b04cf82739552b1516a0b29d20cc53aaae5.

--- drivers/leds/trigger/ledtrig-backlight.c
+++ drivers/leds/trigger/ledtrig-backlight.c
@@ -36,17 +36,9 @@ static int fb_notifier_callback(struct notifier_block *p,
 					struct bl_trig_notifier, notifier);
 	struct led_classdev *led = n->led;
 	struct fb_event *fb_event = data;
-/*  NBQ - MaoyiChou - [NBQ-836] - [Cloud LED] Implement LP5523 driver. */
-	int *blank;
-	int new_status;
+	int *blank = fb_event->data;
+	int new_status = *blank ? BLANK : UNBLANK;
 
-	/* If we aren't interested in this event, skip it immediately ... */
-	if (event != FB_EVENT_BLANK)
-		return 0;
-
-	blank = fb_event->data;
-	new_status = *blank ? BLANK : UNBLANK;
-/* end  NBQ - MaoyiChou - [NBQ-836] */
 	switch (event) {
 	case FB_EVENT_BLANK:
 		if (new_status == n->old_status)
